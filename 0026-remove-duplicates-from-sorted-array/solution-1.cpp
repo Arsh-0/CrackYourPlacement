18b689f649bc151f53d6724d34c23debfd926aa6
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-07-28 03:53:57 +0530

    Update solution-1.cpp

--- 0026-remove-duplicates-from-sorted-array/solution-1.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution-1.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        /* since unique means set, while the question demands for it to be sorted, simply store unique elements and refill the vector with the same. */
+        /* Since unique means set, while the question demands for it to be sorted, simply store unique elements and refill the vector with the same. */
         set <int> s;
         int index = 0;
 
