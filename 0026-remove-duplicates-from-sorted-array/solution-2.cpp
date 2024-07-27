be81f89528a03d059dbf61a6a6a538159fc13c31
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-07-28 03:53:38 +0530

    Update solution-2.cpp

--- 0026-remove-duplicates-from-sorted-array/solution-2.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution-2.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        /* The first solution was using extra space, O(n) in worst case of all elements being unique,
+        Let's make use of the sorted property instead that all the same element will folk together in sorted array.
+        Which means we simply wait to encounter a different element than what we have at current position. */
         int pos = 0;
         for(int i = 1; i < nums.size(); ++i)
         {
