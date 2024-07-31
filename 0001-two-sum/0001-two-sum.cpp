56e27c48ae1999d2da251a20137339395e58d483
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-07-31 17:18:13 +0530

    Time: 8 ms (77.96%), Space: 14.4 MB (20.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map <int, int> m;     // to store <element, index> for keeps tabs on index
         for(int i = 0; i < nums.size(); ++i)
         {
-            int remains = abs(target - nums[i]);
+            int remains = target - nums[i];
             if(m.find(remains) != m.end()) return {m[remains], i};
             else m[nums[i]] = i;
         }
