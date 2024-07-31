a1136f0323a91ced7eb81dae70fd0acd899ff54e
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-07-31 22:30:20 +0530

    Time: 5 ms (28.11%), Space: 11.1 MB (33.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(m == 0) nums1 = nums2;
-        if(n == 0) return;
-        
-        int ptr1 = m - 1, ptr2 = n - 1;
-        for(int i = m + n - 1; i >= 0; --i)
+        int ptr1 = m - 1, ptr2 = n - 1, index = m + n - 1;
+        while(ptr1 >= 0 and ptr2 >= 0)  // starting from back helps us remove extra space from solution before.
         {
-            if(ptr1 < 0 or ptr2 < 0) break;
-            if(nums1[ptr1] > nums2[ptr2]) nums1[i] = nums1[ptr1--];
-            else nums1[i] = nums2[ptr2--];
+            if(nums1[ptr1] > nums2[ptr2]) nums1[index] = nums1[ptr1--];
+            else nums1[index] = nums2[ptr2--];
+
+            index--;
         }
+
+        // This is required only for nums2 because in case of nums1 if ptr1 remains they all are already at their right places.
+        while(ptr2 >= 0) nums1[index--] = nums2[ptr2--];
     }
 };
