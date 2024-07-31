0108d18ce4f2f2155eb21aebaf90be91cd7e5d90
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-07-31 22:31:44 +0530

    Update and rename 0088-merge-sorted-array.cpp to solution-2.cpp

--- 0088-merge-sorted-array/solution-2.cpp
+++ 0088-merge-sorted-array/solution-2.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        /* The solution uses strictly O(m + n) time complexity for a single traversal and O(1) space complexity */
         int ptr1 = m - 1, ptr2 = n - 1, index = m + n - 1;
         while(ptr1 >= 0 and ptr2 >= 0)  // starting from back helps us remove extra space from solution before.
         {
