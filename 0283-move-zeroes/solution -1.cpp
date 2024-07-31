843c7d1d8f3f5814a17ebe3bff74cf88ef128915
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-07-31 16:17:45 +0530

    Update solution -1.cpp

--- 0283-move-zeroes/solution -1.cpp
+++ 0283-move-zeroes/solution -1.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int pos = 0;
+        int pos = 0;    // initial starting position
         for(int i = 0; i < nums.size(); ++i)
         {
-            if(nums[i] != 0) swap(nums[i], nums[pos++]);
+            if(nums[i] != 0) swap(nums[i], nums[pos++]);    // push to front if non - zero, all the zeros will end up behind this way.
         }
     }
 };
