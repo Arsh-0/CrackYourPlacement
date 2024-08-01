453a93d765f40c992b708b53421a2c90034fd867
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-08-01 16:11:36 +0530

    Time: 71 ms (82.68%), Space: 63.9 MB (48.96%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        /* A variation of tortoise and hare algorithm, where we first detect the cycle and once that is done, reset slow pointer while fast will stay where it was
+        we run both pointers at the same pace till they meet again, this is the place which is the beginning of the cycle. */
         int fast = 0, slow = 0;
 
+        // cycle detection
         do
         {
             slow = nums[slow];
@@ -10,6 +13,7 @@ public:
         }
         while(fast != slow);
 
+        // cycle starting point search
         slow = 0;
         while(slow != fast)
         {
