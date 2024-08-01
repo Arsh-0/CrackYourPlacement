d144f87c7471b060036b679a83a3821d28ed239b
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-08-01 21:22:58 +0530

    Time: 17 ms (5.34%), Space: 17.3 MB (5.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int r = matrix.size(), c = matrix[0].size();
-        unordered_set <int> rows, cols;
+        unordered_set <int> rows, cols;     // hashing the rows and cols to be set to 0
         for(int i = 0; i < r; ++i)
         for(int j = 0; j < c; ++j)
         {
@@ -13,6 +13,7 @@ public:
             }
         }
 
+        // set if either row or col was found to be hashed, since we traverse whole 2D array twice, takes O(2 * (m * n)) time complexity.
         for(int i = 0; i < r; ++i)
         for(int j = 0; j < c; ++j)
         {
