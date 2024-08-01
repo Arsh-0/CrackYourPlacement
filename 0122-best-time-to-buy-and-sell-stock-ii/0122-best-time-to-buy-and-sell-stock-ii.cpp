166d6c9027bd759e9198fc1c7bf30fa247860b00
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-08-01 21:33:36 +0530

    Time: 6 ms (53.11%), Space: 15.9 MB (52.88%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,6 +4,7 @@ public:
         int MaxProfit = 0;
         for(int i = 1; i < prices.size(); ++i)
         {
+            // trying to sell it everyday, if there is any profit at all
             if(prices[i - 1] < prices[i]) MaxProfit += prices[i] - prices[i - 1];
         }
 
