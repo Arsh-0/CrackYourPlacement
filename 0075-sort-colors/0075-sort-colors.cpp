ada1ce8ebd6d8a7e66c9b53e0fc50b13b542685a
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-08-01 16:23:28 +0530

    Time: 5 ms (15.76%), Space: 10.1 MB (15.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map <int, int> m;     // will be used to store the frequency of each.
-        for(auto &i : nums) m[i]++;
-
-        int index = 0;  // initial filling position
-        for(int i = 0; i < 3; ++i)  // iterate for 0, 1 and 2
+        /* without using extra O(n) extra space as in brute force, we can simple iterate over the order array of 3 elements,
+        and set the position based off of it, making it O(3 * n) solution i.e. O(n) solution. */
+        
+        vector <int> order = {0, 1, 2};     // all flags with colors
+        int pos = 0;    // initial position or index
+        for(auto &i : order)
+        for(int j = 0; j < nums.size(); ++j)
         {
-            while(m[i]--) nums[index++] = i;    // fill as many times it occured in array.
+            if(i == nums[j]) swap(nums[pos++], nums[j]);
         }
     }
 };
