5b20125be053efdd6b4c7b6bbad7d65293c54f70
Arsh Pathak <151553994+Arsh-0@users.noreply.github.com>
2024-08-01 06:49:46 +0530

    Time: 14 ms (81.74%), Space: 26.9 MB (55.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // because the majority element occurs for half of the list, if sorted must be the element at the mid.
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        /* Brute force takes O(nlogn) for sorting, while applying Moore's voting algorithm, we can optimize this to O(n). */
+        int count = 1, candidate = nums[0];
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            if(candidate == nums[i]) count++;   // if same as candidate, increase support else decrease it
+            else count--;
+
+            if(count == 0)  // once an becomes minority, set the current element as new prospective candidate
+            {
+                candidate = nums[i];
+                count = 1;
+            }
+        }
+
+        return candidate;
     }
 };
